f2df6f6efa4d11bd8a552308f3e3cd6e1322d685
nimayaK <nimaya.kodikara@gmail.com>
2019-07-19 19:28:02 +1000

    add main, calculateDistance functions

--- robot.py
+++ robot.py
@@ -11,10 +11,22 @@ class Robot:
 
 robot = Robot()
 
+def main():
+    print("Woven graduate coding test - Robot CLI App \n")
+    print("Available commands: \n * `F` - move forward 1 unit \n * `B` - move backward 1 unit \n * `R` - turn right 90 degrees \n * `L` - turn left 90 degrees \n example: `F1,R1,B2,L1,B3` \n")
+    command = input("Please enter command: ")
+    commandArray = command.split(",")
+    print(commandArray)
+    calculateDistance()
+
 def calculateDistance():
     p1 = [0, 0]
     p2 = [0, -4]
     distance = math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
     print(distance)
 
-calculateDistance()
+if __name__ == "__main__":
+    main()
+
+
+
